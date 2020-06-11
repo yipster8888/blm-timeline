3a7b12b167c4555c4ac65f6cc035d31c8f066dac
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-06-10 22:45:16 -0600

    Started adding the city filter

--- src/components/CityFilter.js
+++ src/components/CityFilter.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-function CityFilter() {
-  return <div></div>;
+function CityFilter(props) {
+  let options = [];
+  if (props.data.timelineData != null) {
+    options = props.data.timelineData.map((element, index) => {
+      return (
+        <option key={index} value={element.location}>
+          {element.location}
+        </option>
+      );
+    });
+  }
+
+  return (
+    <div className="city-filter">
+      <label className="city-filter-label">Filter by city: </label>
+      <select name="targetCity" onChange={props.handleChangeTargetCity}>
+        {options}
+      </select>
+    </div>
+  );
 }
 
 export default CityFilter;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -11,6 +11,17 @@ class Timeline extends Component {
       timelineData: [],
       targetCity: "",
     };
+
+    //Binding event handlers here
+    this.handleChangeTargetCity = this.handleChangeTargetCity.bind(this);
+  }
+
+  //Changes state to reflect a change to targetCity
+  handleChangeTargetCity(event) {
+    const { value } = event.target;
+    this.setState({
+      targetCity: value,
+    });
   }
 
   //Once the component mounts, we can make the data API call
@@ -24,12 +35,20 @@ class Timeline extends Component {
   }
 
   render() {
+    console.log(this.state.targetCity);
+
     if (this.state.timelineData != null && this.state.timelineData.length > 0) {
       return (
-        <div className="timeline-container">
-          {this.state.timelineData.map((data, index) => (
-            <TimelineItem data={data} key={index} />
-          ))}
+        <div>
+          <div className="timeline-container">
+            {this.state.timelineData.map((data, index) => (
+              <TimelineItem data={data} key={index} />
+            ))}
+          </div>
+          <CityFilter
+            data={this.state}
+            handleChangeTargetCity={this.handleChangeTargetCity}
+          />
         </div>
       );
     }
