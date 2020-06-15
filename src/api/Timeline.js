b5f56caf81dd0a110de248de6c43dc9fb76225cf
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-06-15 14:05:11 -0600

    Revert "fix: adjusted the url for the axios request"
    This reverts commit b3180ab6158ab493ad431b2a284941a5e03df188.

--- src/api/Timeline.js
+++ src/api/Timeline.js
@@ -4,13 +4,13 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export default {
   getPosts: (filters) => {
-    return axios.get(`${API_URL}/timeline`, {
+    return axios.get(`${API_URL}timeline`, {
       params: filters,
     });
   },
 
   insertPost: (data, password) => {
-    return axios.post(`${API_URL}/timeline`, data, {
+    return axios.post(`${API_URL}timeline`, data, {
       headers: {
         authorization: "Bearer " + password,
       },
